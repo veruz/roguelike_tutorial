bea2962af7ea0c13ee770df27655b850368d0a30
veruz <veruzshura666@gmail.com>
2017-10-25 15:42:16 +0300

    Added field of view and "fog of war"

--- render_functions.py
+++ render_functions.py
@@ -1,16 +1,27 @@
-def render_all(con, entities, game_map, root_console, screen_width, screen_height, colors):
-    # draw all map tiles
-    for x,y in game_map:
-        wall = not game_map.transparent[x,y]
+def render_all(con, entities, game_map, fov_recompute, root_console, screen_width, screen_height, colors):
+    if fov_recompute:
+        # draw all map tiles
+        for x,y in game_map:
+            wall = not game_map.transparent[x,y]
 
-        if wall:
-            con.draw_char(x, y, None, fg=None, bg=colors.get('dark wall'))
-        else:
-            con.draw_char(x, y, None, fg=None, bg=colors.get('dark ground'))
+            if game_map.fov[x,y]:
+                if wall:
+                    con.draw_char(x, y, None, fg=None, bg=colors.get('light_wall'))
+                else:
+                    con.draw_char(x, y, None, fg=None, bg=colors.get('light_ground'))
+
+                game_map.explored[x][y] = True
+
+            elif game_map.explored[x][y]:
+                if wall:
+                    con.draw_char(x, y, None, fg=None, bg=colors.get('dark_wall'))
+                else:
+                    con.draw_char(x, y, None, fg=None, bg=colors.get('dark_ground'))
+
 
     # Draw all entities in list
     for entity in entities:
-        draw_entity(con,entity)
+        draw_entity(con,entity, game_map.fov)
 
     root_console.blit(con, 0, 0, screen_width, screen_height)
 
@@ -20,8 +31,9 @@ def clear_all(con, entities):
         clear_entity(con, entity)
 
 
-def draw_entity(con, entity):
-    con.draw_char(entity.x, entity.y, entity.char, entity.color, bg=None)
+def draw_entity(con, entity, fov):
+    if fov[entity.x, entity.y]:
+        con.draw_char(entity.x, entity.y, entity.char, entity.color, bg=None)
 
 
 def clear_entity(con, entity):
