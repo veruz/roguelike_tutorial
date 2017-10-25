b8114be1bdce380804e661e99cf02a83ea86e501
veruz <veruzshura666@gmail.com>
2017-10-25 14:55:51 +0300

    Added procedurally generated rooms and tunnels

--- engine.py
+++ engine.py
@@ -12,13 +12,17 @@ def main():
     map_width = 80
     map_height = 45
 
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+
     colors = {
         'dark wall': (0, 0, 100),
         'dark ground': (50, 50, 150)
     }
 
     player = Entity(int(screen_width / 2), int(screen_height / 2), '@', (255, 255, 255))
-    npc = Entity(int(screen_width /2 -5), int(screen_height /2), '@', (255, 255, 0))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', (255, 255, 0))
     entities = [npc, player]
 
     tdl.set_font('arial10x10.png', greyscale=True, altLayout=True)
@@ -27,7 +31,7 @@ def main():
     con = tdl.Console(screen_width, screen_height)
 
     game_map = tdl.map.Map(map_width, map_height)
-    make_map(game_map)
+    make_map(game_map, max_rooms, room_min_size, room_max_size, map_width, map_height, player)
 
     while not tdl.event.is_window_closed():
         render_all(con, entities, game_map, root_console, screen_width, screen_height, colors)
@@ -53,14 +57,15 @@ def main():
 
         if move:
             dx, dy = move
-            if game_map.walkable[player.x +dx, player.y + dy]:
+            if game_map.walkable[player.x + dx, player.y + dy]:
                 player.move(dx, dy)
 
         if exit:
             return True
-        
+
         if fullscreen:
             tdl.set_fullscreen(not tdl.get_fullscreen())
 
+
 if __name__ == '__main__':
-    main() 
+    main()
