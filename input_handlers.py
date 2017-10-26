b18628fe06ca3f2d7b96e82159fb85d9339d9bbe
veruz <veruzshura666@gmail.com>
2017-10-26 15:40:28 +0300

    Fixed error with textwrapping, added inventory and basic item functionality.

--- input_handlers.py
+++ input_handlers.py
@@ -1,15 +1,45 @@
-def handle_keys(user_input):
+from game_states import GameStates
+
+
+def handle_keys(user_input, game_state):
+    if game_state == GameStates.PLAYER_TURN:
+        return handle_player_turn_keys(user_input)
+    elif game_state == GameStates.PLAYER_DEAD:
+        return handle_player_dead_keys(user_input)
+    elif game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
+        return handle_inventory_keys(user_input)
+    return {}
+
+
+def handle_inventory_keys(user_input):
+    if not user_input.char:
+        return {}
+
+    index = ord(user_input.char) - ord('a')
+
+    if index >= 0:
+        return {'inventory_index': index}
+
+    if user_input.key == 'ENTER' and user_input.alt:
+        # Alt+Enter: Fullscreen
+        return {'fullscreen': True}
+    elif user_input.key == 'ESCAPE':
+        # Exit game
+        return {'exit': True}
+
+
+def handle_player_turn_keys(user_input):
     key_char = user_input.char
 
     # Movement keys
     if user_input.key == 'UP' or key_char == 'k':
-        return {'move': (0,-1)}
+        return {'move': (0, -1)}
     elif user_input.key == 'DOWN' or key_char == 'j':
         return {'move': (0, 1)}
     elif user_input.key == 'LEFT' or key_char == 'h':
-        return {'move': (-1,0)}
+        return {'move': (-1, 0)}
     elif user_input.key == 'RIGHT' or key_char == 'l':
-        return {'move': (1,0)}
+        return {'move': (1, 0)}
     elif key_char == 'UP':
         return {'move': (-1, -1)}
     elif key_char == 'u':
@@ -19,6 +49,13 @@ def handle_keys(user_input):
     elif key_char == 'n':
         return {'move': (1, 1)}
 
+    if key_char == 'g':
+        return {'pickup': True}
+    elif key_char == 'i':
+        return {'show_inventory': True}
+    elif key_char == 'd':
+        return {'drop_inventory': True}
+
     if user_input.key == 'ENTER' and user_input.alt:
         # Alt+Enter: Fullscreen
         return {'fullscreen': True}
@@ -27,4 +64,20 @@ def handle_keys(user_input):
         return {'exit': True}
 
     # No key pressed
+    return {}
+
+
+def handle_player_dead_keys(user_input):
+    key_char = user_input.char
+
+    if key_char == 'i':
+        return {'show_inventory': True}
+
+    if user_input.key == 'ENTER' and user_input.alt:
+        # alt+enter - fullscreen
+        return {'fullscreen': True}
+    elif user_input.key == 'ESCAPE':
+        # exit game
+        return {'exit': True}
+
     return {}
